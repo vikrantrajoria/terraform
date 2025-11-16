b7b94f6a9a574f7b44f1ab7c8e2c514a4d6f854b
vikrantrajoria <165982818+vikrantrajoria@users.noreply.github.com>
2025-11-16 09:33:04 +0530

    Configure AWS provider and add EC2 instance
    Added AWS provider configuration and an EC2 instance resource.

--- code1.tf
+++ code1.tf
@@ -1,1 +1,11 @@
-
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIASQJJCXUDJFFLLNWY"
+  secret_key = "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"
+}
+resource "aws_instance" "first" {
+  ami           = "ami-08d96a04ebb630b47"
+  instance_type = "t3.small"
+  tags = {
+    Name = "first"
+  }}
